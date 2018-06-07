0b6d8ebabc0d6c0f3071b9c1f749ead42a5f77e8
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-06-06 22:17:50 -0400

    scraping

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,64 @@
+var request = require('request');
+var cheerio = require("cheerio");
+
+var db = require("../models");
+
 module.exports = function (app) {
     //login page
     app.get("/", (req, res) => {
         res.render("index");
     });
+
+
+    app.get("/scrape", (req, res) => {
+        request.get("https://www.delish.com/cooking/", (error, response, body) => {
+            //load html into cheerio and save as $
+            var $ = cheerio.load(body);
+            $(".full-item").each((i, element) => {
+
+                //can replace element with this
+
+                var title = $(element).find(".full-item-title").text().trim();
+
+                var description = $(element).find(".full-item-dek p").text().trim();
+
+                var link = "https://www.delish.com" + $(element).find(".full-item-title").attr("href");
+
+                var category = $(element).find(".item-parent-link").text().trim();
+
+
+
+                // console.log(`${category}
+                // ${link}
+                // ${title}
+                // ${description}`);
+                //If want to distinguish based on category
+                // switch (category) {
+                //     case "Recipes":
+                //       console.log("This is a recipe");
+                //     case "Meals & Cooking":
+                //         console.log("Meals and Cooking");
+                // }
+
+                //create result object with properties I want to grab
+                var result = {
+                    title: title,
+                    description: description,
+                    link: link,
+                    category: category,
+                }
+
+                //WANT TO CHECK AND MAKE SURE ITS NEW ARTICLE BEFORE SAVING
+                //create new Article
+                db.Article.create(result).then(article => {
+                    console.log(article);
+                }).catch(err => {
+                    return res.json(err);
+                })
+            });
+        });
+        //for now just rendering main page
+        res.render("index");
+    });
+
 }
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var cheerio = require("cheerio");
-// var request? 
+
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -34,7 +33,7 @@ require("./routes/html-routes.js")(app);
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
+
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
